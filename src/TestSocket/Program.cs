97f1e8b6169e320bd106b45a7e50bd1d813f1cbf
iamsorryprincess <nickpank112@yandex.ru>
2022-03-22 13:25:15 +0300

    test socket

---
+++ src/TestSocket/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TestSocket
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(new IPEndPoint(IPAddress.Any, 9090));
+            socket.Listen(10);
+            Console.WriteLine("Listening");
+
+            while (true)
+            {
+                var handler = socket.Accept();
+                var body = $"<body><h1>${handler.RemoteEndPoint}</h1></body>";
+
+                var response = @$"HTTP/1.1 200 Ok
+Server: test
+Content-Type: text/html
+Connection: close
+Content-Length: {body.Length}
+
+{body}";
+                
+                handler.Send(Encoding.UTF8.GetBytes(response));
+                handler.Close();
+            }
+        }
+    }
+}
