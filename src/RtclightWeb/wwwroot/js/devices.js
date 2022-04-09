fe69cc1f20226559bf4fbe6e59abb67f821f5702
iamsorryprincess <nickpank112@yandex.ru>
2022-04-09 19:17:59 +0300

    test ice

--- src/RtclightWeb/wwwroot/js/devices.js
+++ src/RtclightWeb/wwwroot/js/devices.js
@@ -1,10 +1,24 @@
 const servers = {
   iceServers: [
     {
-      urls: ['stun:185.189.167.147:19302'],
-    }
+      urls: "stun:openrelay.metered.ca:80",
+    },
+    {
+      urls: "turn:openrelay.metered.ca:80",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+    {
+      urls: "turn:openrelay.metered.ca:443",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
+    {
+      urls: "turn:openrelay.metered.ca:443?transport=tcp",
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
   ],
-  iceCandidatePoolSize: 10,
 };
 
 const devicesPc = new RTCPeerConnection(servers);
